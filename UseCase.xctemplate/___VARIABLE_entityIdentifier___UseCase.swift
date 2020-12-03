bd45612890e87ade6f714f1c318c17e5feb2f143
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-12-03 09:57:36 +0700

    Add new template for UseCase

---
+++ UseCase.xctemplate/___VARIABLE_entityIdentifier___UseCase.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ All rights reserved.
+//
+
+import Foundation
+
+public struct ___VARIABLE_entityIdentifier___UseCaseRequestValue {
+
+}
+
+public protocol ___VARIABLE_entityIdentifier___UseCase {
+
+}
+
+public final class Default___VARIABLE_entityIdentifier___UseCase {
+
+    public init() {
+    }
+
+}
+
+extension Default___VARIABLE_entityIdentifier___UseCase: ___VARIABLE_entityIdentifier___UseCase {
+
+}
