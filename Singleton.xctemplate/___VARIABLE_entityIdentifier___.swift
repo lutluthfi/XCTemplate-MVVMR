bf04c2100dca552cf69c929bdb3b6c85077c96e9
Arif Luthfiansyah <lutluthfi@icloud.com>
2021-02-11 14:18:47 +0700

    Add Singleton

---
+++ Singleton.xctemplate/___VARIABLE_entityIdentifier___.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ All rights reserved.
+//
+
+import Foundation
+
+public protocol ___VARIABLE_entityIdentifier___Protocol {
+
+}
+
+public final class ___VARIABLE_entityIdentifier___ {
+
+    public static shared: ___VARIABLE_entityIdentifier___Protocol = ___VARIABLE_entityIdentifier___()
+    
+    private init() {
+    }
+
+}
+
+extension ___VARIABLE_entityIdentifier___: ___VARIABLE_entityIdentifier___Protocol {
+    
+}
