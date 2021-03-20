5c2927d0282bfc820d9006a205532b0b72e25fff
Arif Luthfiansyah <lutluthfi@icloud.com>
2021-03-20 20:10:12 +0700

    [UPDATE] Remove NavigationBar and NavigationItem from View constructor

--- UI+FlowCoordinator.xctemplate/___VARIABLE_sceneIdentifier___View.swift
+++ UI+FlowCoordinator.xctemplate/___VARIABLE_sceneIdentifier___View.swift
@@ -14,14 +14,12 @@ protocol ___VARIABLE_sceneIdentifier___ViewDelegate: AnyObject {
 
 // MARK: ___VARIABLE_sceneIdentifier___ViewFunction
 protocol ___VARIABLE_sceneIdentifier___ViewFunction {
-    func viewWillAppear()
+    func viewWillAppear(navigationController: UINavigationController?, tabBarController: UITabBarController?)
     func viewWillDisappear()
 }
 
 // MARK: ___VARIABLE_sceneIdentifier___ViewSubview
 protocol ___VARIABLE_sceneIdentifier___ViewSubview {
-    var navigationBar: UINavigationBar? { get }
-    var navigationItem: UINavigationItem! { get }
 }
 
 // MARK: ___VARIABLE_sceneIdentifier___ViewVariable
@@ -47,9 +45,7 @@ final class Default___VARIABLE_sceneIdentifier___View: UIView, ___VARIABLE_scene
         fatalError("init(coder:) has not been implemented")
     }
 
-    init(navigationBar: UINavigationBar?, navigationItem: UINavigationItem) {
-        self.navigationBar = navigationBar
-        self.navigationItem = navigationItem
+    init() {
         super.init(frame: UIScreen.main.fixedCoordinateSpace.bounds)
         self.subviewDidInit()
         self.subviewConstraintDidInit()
@@ -76,7 +72,7 @@ extension Default___VARIABLE_sceneIdentifier___View {
 // MARK: Input Function
 extension Default___VARIABLE_sceneIdentifier___View {
     
-    func viewWillAppear() {
+    func viewWillAppear(navigationController: UINavigationController?, tabBarController: UITabBarController?) {
         
     }
     
