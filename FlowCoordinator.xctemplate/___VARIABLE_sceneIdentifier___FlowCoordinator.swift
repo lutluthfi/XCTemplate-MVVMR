dbd8f30fdd548b377f3b7742bc97164595b50eb4
Arif Luthfiansyah <lutluthfi@icloud.com>
2021-04-05 14:38:42 +0700

    [UPDATE] Set public for FlowCoordinator

--- FlowCoordinator.xctemplate/___VARIABLE_sceneIdentifier___FlowCoordinator.swift
+++ FlowCoordinator.xctemplate/___VARIABLE_sceneIdentifier___FlowCoordinator.swift
@@ -10,29 +10,29 @@ import Foundation
 import UIKit
 
 // MARK: ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory
-protocol ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory  {
+public protocol ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory  {
     func make___VARIABLE_sceneIdentifier___ViewController() -> UIViewController
 }
 
 // MARK: ___VARIABLE_sceneIdentifier___FlowCoordinator
-protocol ___VARIABLE_sceneIdentifier___FlowCoordinator {
+public protocol ___VARIABLE_sceneIdentifier___FlowCoordinator {
     func start(with instructor: ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor)
 }
 
 // MARK: ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor
-enum ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor {
+public enum ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor {
     
 }
 
 // MARK: Default___VARIABLE_sceneIdentifier___FlowCoordinator
-final class Default___VARIABLE_sceneIdentifier___FlowCoordinator {
+public final class Default___VARIABLE_sceneIdentifier___FlowCoordinator {
 
     // MARK: DI Variable
     let navigationController: UINavigationController
     let factory: ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory
 
     // MARK: Init Funciton
-    init(navigationController: UINavigationController, factory: ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory) {
+    public init(navigationController: UINavigationController, factory: ___VARIABLE_sceneIdentifier___FlowCoordinatorFactory) {
         self.navigationController = navigationController
         self.factory = factory
     }
@@ -41,7 +41,7 @@ final class Default___VARIABLE_sceneIdentifier___FlowCoordinator {
 
 extension Default___VARIABLE_sceneIdentifier___FlowCoordinator: ___VARIABLE_sceneIdentifier___FlowCoordinator {
     
-    func start(with instructor: ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor) {
+    public func start(with instructor: ___VARIABLE_sceneIdentifier___FlowCoordinatorInstructor) {
     }
     
 }
